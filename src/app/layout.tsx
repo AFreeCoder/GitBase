d6b0dee4474bfbcfec53ca64b6d9fdbe74477613
AFreeCoder <afreecoder@163.com>
2024-12-02 13:40:43 +0800

    增加统计代码

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,9 +2,9 @@ import './globals.css'
 import { Inter } from 'next/font/google'
 import { Layout } from '@/components/Layout'
 import { Metadata } from 'next'
+import Script from 'next/script'
 
 const inter = Inter({ subsets: ['latin'] })
-
 
 export const metadata: Metadata = {
   title: {
@@ -20,8 +20,29 @@ interface RootLayoutProps {
 
 export default function RootLayout({ children }: RootLayoutProps) {
   return (
-    <html lang="en">
+    <html lang="zh-CN">
+      <head>
+        <Script 
+          defer
+          data-domain="claudehelp.com"
+          src="https://app.pageview.app/js/script.js"
+          strategy="afterInteractive"
+        />
+      </head>
       <body className={inter.className}>
+        <Script 
+          src="https://www.googletagmanager.com/gtag/js?id=G-MFH40S2FRF"
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+
+            gtag('config', 'G-MFH40S2FRF');
+          `}
+        </Script>
         <Layout>{children}</Layout>
       </body>
     </html>
